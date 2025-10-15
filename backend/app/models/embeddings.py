0857da7e7ac41b2dcffaa09c19f896e2b723ee06
Akash A S <akash.as@zebra.com>
2025-10-15 20:22:20 +0530

    Made indentation changes in embeddings.py

--- backend/app/models/embeddings.py
+++ backend/app/models/embeddings.py
@@ -5,11 +5,16 @@ from app.db.base import Base
 import uuid
 from pgvector.sqlalchemy import Vector
 
+
 class Embeddings(Base):
     __tablename__ = "embeddings"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
-    ticket_id = Column(UUID(as_uuid=True), ForeignKey("historical_tickets.id", ondelete="CASCADE"), nullable=False)
+    ticket_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("historical_tickets.id", ondelete="CASCADE"),
+        nullable=False,
+    )
     vector = Column(Vector(1536), nullable=False)
 
     # Relationship
@@ -17,8 +22,12 @@ class Embeddings(Base):
 
     # Table arguments for indexing and constraints
     __table_args__ = (
-        Index("ix_embeddings_ticket_id", "ticket_id"), # Index for fast lookup by ticket_id
-        Index("ix_embeddings_vector", "vector", postgresql_using="ivfflat") # Index for vector similarity search
+        Index(
+            "ix_embeddings_ticket_id", "ticket_id"
+        ),  # Index for fast lookup by ticket_id
+        Index(
+            "ix_embeddings_vector", "vector", postgresql_using="ivfflat"
+        ),  # Index for vector similarity search
     )
     """
     Indexing
