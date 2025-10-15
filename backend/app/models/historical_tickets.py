1f0d46464ae1e26606512095c3986dcaead4aad8
Akash A S <akash.as@zebra.com>
2025-10-15 20:19:58 +0530

    Made indentation changes

--- backend/app/models/historical_tickets.py
+++ backend/app/models/historical_tickets.py
@@ -5,12 +5,14 @@ from app.db.base import Base
 from enum import Enum
 import uuid
 
+
 class Priority(str, Enum):
     CRITICAL = "1 - Critical"
     HIGH = "2 - High"
     MEDIUM = "3 - Medium"
     LOW = "4 - Low"
     PLANNING = "5 - Planning"
+
 
 class HistoricalTicket(Base):
     __tablename__ = "historical_tickets"
@@ -19,22 +21,23 @@ class HistoricalTicket(Base):
     snow_id = Column(String(50), unique=True, nullable=False)
     title = Column(String(500), nullable=False)
     description = Column(Text, nullable=True)
-    priority =  Column(PgEnum(
-        Priority,
-        name = "priority",
-        create_type=False
-    ))
+    priority = Column(PgEnum(Priority, name="priority", create_type=False))
     status = Column(String(50), nullable=False)
-    # metadata = 
-    created_at = Column(DateTime(timezone=True), nullable=False,
-                        )
+    # metadata =
+    created_at = Column(DateTime(timezone=True), nullable=False)
     resolved_at = Column(DateTime(timezone=True), nullable=True)
-    
+
     # Foreign Keys
-    assignee_id = Column(UUID(as_uuid=True), ForeignKey("team_members.id", ondelete="SET NULL"), nullable=True)
+    assignee_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("team_members.id", ondelete="SET NULL"),
+        nullable=True,
+    )
     assignee = relationship("TeamMember", back_populates="tickets")
 
     # Assignment History?
 
     # Embeddings
-    embeddings = relationship("Embeddings", back_populates="ticket_embeddings", cascade="all, delete-orphan")
+    embeddings = relationship(
+        "Embeddings", back_populates="ticket_embeddings", cascade="all, delete-orphan"
+    )
