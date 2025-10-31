ad2ee6b79881bd3c6186918960ce83abee1ed079
Akash A S <akash.as@zebra.com>
2025-10-31 15:09:47 +0530

    fix: Updated FK'S

--- backend/app/models/skills.py
+++ backend/app/models/skills.py
@@ -1,14 +1,13 @@
-from enum import Enum
 from app.db.base import Base
-from sqlalchemy import Column,Text,String, ForeignKey, DateTime, Numeric
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
-import datetime
 import uuid
 
 class Skill(Base):
     __tablename__ = "skills"
 
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
     name = Column(String(100), unique=True, nullable=False, index=True)
     category = Column(String(100), nullable=False, index=True)
 
@@ -19,8 +18,9 @@ class Skill(Base):
 class TeamMemberSkill(Base):
     __tablename__ = "team_member_skills"
 
-    member_id = Column(UUID(as_uuid=True), ForeignKey("team_members.id"), ondelete="CASCADE", nullable=False, index=True)
-    skill_id = Column(UUID(as_uuid=True), ForeignKey("skills.id"), ondelete="CASCADE", nullable=False, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
+    member_id = Column(UUID(as_uuid=True), ForeignKey("team_members.id", ondelete="CASCADE"), nullable=False, index=True)
+    skill_id = Column(UUID(as_uuid=True), ForeignKey("skills.id", ondelete="CASCADE"), nullable=False, index=True)
 
     # Relationship
     skill = relationship("Skill", back_populates="member_skills")
